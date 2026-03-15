d5fad120b40a4c326d9ed120d20df4d2f8b45ef9
AlejandroMorenoBernal-lab <alejandro.morenob@um.es>
2026-03-16 00:54:30 +0100

    Configuracion del proyecto Maven con SpringBoot y las dependencias de JavaFX

---
+++ src/main/java/trelloclone/TrelloCloneApplication.java
@@ -0,0 +1,13 @@
+package trelloclone;
+
+import javafx.application.Application;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrelloCloneApplication {
+
+    public static void main(String[] args) {
+        Application.launch(JavaFxApplication.class, args);
+    }
+
+}
